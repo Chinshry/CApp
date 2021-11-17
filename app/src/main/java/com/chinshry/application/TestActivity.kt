cc0a7dc047771c0eefe14c3b30eba571ffd3dbcf
chinshry <cs616520704@gmail.com>
2021-11-18 02:37:08 +0800

    add PictureSelector

--- app/src/main/java/com/chinshry/application/TestActivity.kt
+++ app/src/main/java/com/chinshry/application/TestActivity.kt
@@ -1,20 +1,41 @@
 package com.chinshry.application
 
+import android.Manifest
+import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.fastjson.JSON
+import com.blankj.utilcode.util.ColorUtils
+import com.blankj.utilcode.util.PermissionUtils
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.chinshry.application.bean.Router
-import kotlinx.android.synthetic.main.activity_test.*
-import android.text.method.ScrollingMovementMethod
-import android.widget.Toast
 import com.chinshry.application.util.DevicesCheckUtil
+import com.chinshry.application.util.GlideEngine
+import com.luck.picture.lib.PictureMediaScannerConnection
+import com.luck.picture.lib.PictureSelectionModel
+import com.luck.picture.lib.PictureSelector
+import com.luck.picture.lib.config.PictureConfig
+import com.luck.picture.lib.config.PictureMimeType
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.listener.OnResultCallbackListener
+import com.luck.picture.lib.tools.MediaUtils
+import kotlinx.android.synthetic.main.activity_test.*
+import com.luck.picture.lib.thread.PictureThreadUtils
+import java.io.File
 
 
 @Route(path = Router.ACTIVITY_TEST)
 class TestActivity : AppCompatActivity() {
 
     companion object {
-        // const val TAG = "TestActivity"
+        const val TAG = "TestActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +45,180 @@ class TestActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        tv_msg.movementMethod = ScrollingMovementMethod.getInstance()
-
-        btn_test.setOnClickListener {
+        btn_root.setOnClickListener {
             if (!DevicesCheckUtil.checkRootAndEmulator(this)) {
                 Toast.makeText(this, "您的设备未root，且为真机", Toast.LENGTH_LONG).show()
             }
         }
+
+        btn_gallery.setOnClickListener {
+            PermissionUtils.permission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .callback { isAllGranted, _, _, _ ->
+                    if (isAllGranted) {
+                        gallerySelect()
+                        return@callback
+                    }
+                }
+                .request()
+        }
+
+        btn_camera.setOnClickListener {
+            PermissionUtils.permission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA)
+                .callback { isAllGranted, _, _, _ ->
+                    if (isAllGranted) {
+                        takePicture()
+                        return@callback
+                    }
+                }
+                .request()
+        }
+
+        btn_clear_cache.setOnClickListener {
+            clearCache()
+        }
+    }
+
+    /**
+     * 创建自定义拍照输出目录
+     *
+     * @return
+     */
+    private fun createCustomCameraOutPath(): String {
+        val customFile = getCacheFilePath() ?: return ""
+        if (!customFile.exists()) {
+            customFile.mkdirs()
+        }
+        return customFile.absolutePath + File.separator
+    }
+
+    private fun getCacheFilePath(): File? {
+        val externalFilesDir: File? = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        externalFilesDir?.absolutePath?.let {
+            return File(it)
+        }
+        return null
+    }
+
+    private fun clearCache() {
+        val dirPictures: File = getCacheFilePath() ?: return
+        val files = dirPictures.listFiles() ?: return
+
+        for (file in files) {
+            if (file.isFile) {
+                val isResult = file.delete()
+                if (isResult) {
+                    PictureThreadUtils.runOnUiThread {
+                        PictureMediaScannerConnection(
+                            this,
+                            file.absolutePath
+                        )
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun gallerySelect() {
+        val gallerySelector = PictureSelector.create(this)
+            .openGallery(PictureMimeType.ofImage())
+            .imageEngine(GlideEngine.createGlideEngine())
+            .selectionMode(PictureConfig.SINGLE) // 单选
+            .isPageStrategy(true, false) // 开启分页模式，默认开启另提供两个参数；pageSize每页总数；isFilterInvalidFile是否过滤损坏图片
+            .isCamera(false) // 列表是否显示拍照按钮
+
+        setCropConfig(gallerySelector)
+    }
+
+    private fun takePicture() {
+        // 单独拍照
+        val cameraSelector = PictureSelector.create(this)
+            .openCamera(PictureMimeType.ofImage())
+            .imageEngine(GlideEngine.createGlideEngine())
+            .setOutputCameraPath(createCustomCameraOutPath()) // 自定义相机输出目录
+
+        setCropConfig(cameraSelector)
+    }
+
+    private fun setCropConfig(pictureSelectionModel: PictureSelectionModel) {
+        pictureSelectionModel
+            .isEnableCrop(true) //是否开启裁剪
+            .cropImageWideHigh(200, 200) // 裁剪宽高比，设置如果大于图片本身宽高则无效
+            .withAspectRatio(1, 1) // 裁剪比例
+            .setCropDimmedColor(ColorUtils.getColor(R.color.half_black))// 设置圆形裁剪背景色值
+            // .freeStyleCropMode(OverlayView.FREESTYLE_CROP_MODE_ENABLE_WITH_PASS_THROUGH) // 裁剪框拖动模式
+            // .isCropDragSmoothToCenter(true) // 裁剪框拖动时图片自动跟随居中
+            .forResult(MyResultCallback(this, iv_preview))
+    }
+
+    /**
+     * 返回结果回调
+     */
+    private class MyResultCallback(val activity: Activity, val imageView: ImageView) :
+        OnResultCallbackListener<LocalMedia> {
+
+        private fun showPreview(media: LocalMedia) {
+            val path: String = if (media.isCut && !media.isCompressed) {
+                // 裁剪过
+                media.cutPath
+            } else if (media.isCut || media.isCompressed) {
+                // 压缩过,或者裁剪同时压缩过,以最终压缩过图片为准
+                media.compressPath
+            } else {
+                // 原图
+                media.path
+            }
+
+            Glide.with(activity)
+                .load(
+                    if (PictureMimeType.isContent(path) && !media.isCut && !media.isCompressed)
+                        Uri.parse(path)
+                    else
+                        path
+                )
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView)
+        }
+
+        override fun onResult(result: List<LocalMedia>) {
+            Log.i(TAG, "返回数据: " + JSON.toJSONString(result))
+
+            for (media in result) {
+                if (media.width == 0 || media.height == 0) {
+                    if (PictureMimeType.isHasImage(media.mimeType)) {
+                        val imageExtraInfo = MediaUtils.getImageSize(media.path)
+                        media.width = imageExtraInfo.width
+                        media.height = imageExtraInfo.height
+                    }
+                }
+
+                showPreview(media)
+
+                Log.i(TAG, "文件名: " + media.fileName)
+                Log.i(TAG, "是否压缩:" + media.isCompressed)
+                Log.i(TAG, "压缩:" + media.compressPath)
+                Log.i(TAG, "原图:" + media.path)
+                Log.i(TAG, "绝对路径:" + media.realPath)
+                Log.i(TAG, "是否裁剪:" + media.isCut)
+                Log.i(TAG, "裁剪:" + media.cutPath)
+                Log.i(TAG, "是否开启原图:" + media.isOriginal)
+                Log.i(TAG, "原图路径:" + media.originalPath)
+                Log.i(TAG, "Android Q 特有Path:" + media.androidQToPath)
+                Log.i(TAG, "宽高: " + media.width + "x" + media.height)
+                Log.i(TAG, "Size: " + media.size)
+                Log.i(TAG, "onResult: $media")
+            }
+
+        }
+
+        override fun onCancel() {
+            Log.i(TAG, "PictureSelector Cancel")
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/chinshry/application/util/GlideEngine.kt
@@ -0,0 +1,189 @@
+package com.chinshry.application.util
+
+import com.luck.picture.lib.engine.ImageEngine
+import com.bumptech.glide.Glide
+import com.luck.picture.lib.widget.longimage.SubsamplingScaleImageView
+import com.luck.picture.lib.listener.OnImageCompleteCallback
+import com.bumptech.glide.request.target.CustomTarget
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import com.luck.picture.lib.tools.MediaUtils
+import com.luck.picture.lib.widget.longimage.ImageSource
+import com.luck.picture.lib.widget.longimage.ImageViewState
+import android.graphics.PointF
+import com.bumptech.glide.request.target.BitmapImageViewTarget
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.request.transition.Transition
+import com.chinshry.application.R
+
+/**
+ * @author：luck
+ * @date：2019-11-13 17:02
+ * @describe：Glide加载引擎
+ */
+class GlideEngine private constructor() : ImageEngine {
+    /**
+     * 加载图片
+     *
+     * @param context   上下文
+     * @param url       资源url
+     * @param imageView 图片承载控件
+     */
+    override fun loadImage(context: Context, url: String, imageView: ImageView) {
+        if (!assertValidRequest(context)) {
+            return
+        }
+        Glide.with(context)
+            .load(url)
+            .into(imageView)
+    }
+
+    /**
+     * 加载网络图片适配长图方案
+     *
+     * @param context       上下文
+     * @param url           资源url
+     * @param imageView     图片承载控件
+     * @param longImageView 长图承载控件
+     * @param callback      网络图片加载回调监听
+     */
+    override fun loadImage(
+        context: Context, url: String,
+        imageView: ImageView, longImageView: SubsamplingScaleImageView,
+        callback: OnImageCompleteCallback
+    ) {
+        if (!assertValidRequest(context)) {
+            return
+        }
+        Glide.with(context)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap?>() {
+                override fun onLoadStarted(placeholder: Drawable?) {
+                    super.onLoadStarted(placeholder)
+                    callback.onShowLoading()
+                }
+
+                override fun onLoadFailed(errorDrawable: Drawable?) {
+                    super.onLoadFailed(errorDrawable)
+                    callback.onHideLoading()
+                }
+
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: Transition<in Bitmap?>?
+                ) {
+                    callback.onHideLoading()
+                    val eqLongImage = MediaUtils.isLongImg(
+                        resource.width,
+                        resource.height
+                    )
+                    longImageView.visibility = if (eqLongImage) View.VISIBLE else View.GONE
+                    imageView.visibility = if (eqLongImage) View.GONE else View.VISIBLE
+                    if (eqLongImage) {
+                        // 加载长图
+                        longImageView.isQuickScaleEnabled = true
+                        longImageView.isZoomEnabled = true
+                        longImageView.setDoubleTapZoomDuration(100)
+                        longImageView.setMinimumScaleType(SubsamplingScaleImageView.SCALE_TYPE_CENTER_CROP)
+                        longImageView.setDoubleTapZoomDpi(SubsamplingScaleImageView.ZOOM_FOCUS_CENTER)
+                        longImageView.setImage(
+                            ImageSource.cachedBitmap(resource),
+                            ImageViewState(0F, PointF(0F, 0F), 0)
+                        )
+                    } else {
+                        // 普通图片
+                        imageView.setImageBitmap(resource)
+                    }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+            })
+    }
+
+    /**
+     * 加载相册目录
+     *
+     * @param context   上下文
+     * @param url       图片路径
+     * @param imageView 承载图片ImageView
+     */
+    override fun loadFolderImage(context: Context, url: String, imageView: ImageView) {
+        if (!assertValidRequest(context)) {
+            return
+        }
+        Glide.with(context)
+            .asBitmap()
+            .load(url)
+            .override(180, 180)
+            .centerCrop()
+            .sizeMultiplier(0.5f)
+            .placeholder(R.drawable.picture_image_placeholder)
+            .into(object : BitmapImageViewTarget(imageView) {
+                override fun setResource(resource: Bitmap?) {
+                    val circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(context.resources, resource)
+                    circularBitmapDrawable.cornerRadius = 8f
+                    imageView.setImageDrawable(circularBitmapDrawable)
+                }
+            })
+    }
+
+    /**
+     * 加载图片列表图片
+     *
+     * @param context   上下文
+     * @param url       图片路径
+     * @param imageView 承载图片ImageView
+     */
+    override fun loadGridImage(context: Context, url: String, imageView: ImageView) {
+        if (!assertValidRequest(context)) {
+            return
+        }
+        Glide.with(context)
+            .load(url)
+            .override(200, 200)
+            .centerCrop()
+            .placeholder(R.drawable.picture_image_placeholder)
+            .into(imageView)
+    }
+
+    companion object {
+        private var instance: GlideEngine? = null
+
+        fun assertValidRequest(context: Context?): Boolean {
+            if (context is Activity) {
+                return !isDestroy(context)
+            } else if (context is ContextWrapper) {
+                if (context.baseContext is Activity) {
+                    val activity = context.baseContext as Activity
+                    return !isDestroy(activity)
+                }
+            }
+            return true
+        }
+
+        private fun isDestroy(activity: Activity?): Boolean {
+            return if (activity == null) {
+                true
+            } else activity.isFinishing || activity.isDestroyed
+        }
+
+        fun createGlideEngine(): GlideEngine? {
+            if (null == instance) {
+                synchronized(GlideEngine::class.java) {
+                    if (null == instance) {
+                        instance = GlideEngine()
+                    }
+                }
+            }
+            return instance
+        }
+    }
+}
