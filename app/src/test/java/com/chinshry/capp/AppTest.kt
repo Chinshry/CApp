3d10b777043878a9f642c1f1bf718b9fb2f74cf2
chinshry <cs616520704@gmail.com>
2023-02-10 02:56:16 +0800

    Optimize： 添加单元测试

---
+++ app/src/test/java/com/chinshry/capp/AppTest.kt
@@ -0,0 +1,16 @@
+package com.chinshry.capp
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by chinshry on 2023/2/10.
+ * Describe：
+ */
+class AppTest {
+    @Test
+    fun testMathRound() {
+        println("test")
+        Assert.assertEquals(3.14f, 3.1415926)
+    }
+}
